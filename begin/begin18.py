df3c14a7f0ccd3838c4d5010212227831af86774
Timyr774 <timur21122010@mail.ru>
2026-09-20 23:13:57 +0300

    Add files via upload

---
+++ begin/begin18.py
@@ -0,0 +1,7 @@
+A = int(input("Введите первую точку: "))
+B = int(input("Введите вторую точку: "))
+C = int(input("Введите третью точку: "))
+
+ac = abs(A - C)
+bc = abs(B - C)
+print(ac * bc)
---
+++ begin/begin4 .py
@@ -0,0 +1,4 @@
+import math
+d = float(input("Введите диаметр окружности: "))
+L = math.pi * d
+print(f"Длинна окружности: {L}")
